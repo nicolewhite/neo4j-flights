b102e23946d71baa43daab24e7978367e8d13a6c
Tim Ojo <ojo.tim@gmail.com>
2015-08-01 20:06:41 -0400

    using the p2neo 2.0 graph interface

--- schema.py
+++ schema.py
@@ -1,13 +1,14 @@
-from py2neo import neo4j
+from py2neo import Graph
 
-graph = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+graph = Graph()
 
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (a:Airport) ASSERT a.id IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (s:State) ASSERT s.name IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (c:City) ASSERT c.name IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (a:Aircraft) ASSERT a.tail_num IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (c:Carrier) ASSERT c.id IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE CONSTRAINT ON (r:Reason) ASSERT r.name IS UNIQUE;").run()
-neo4j.CypherQuery(graph, "CREATE INDEX ON :Flight(flight_num);").run()
-neo4j.CypherQuery(graph, "CREATE INDEX ON :Airport(abbr);").run()
-neo4j.CypherQuery(graph, "CREATE INDEX ON :Carrier(abbr);").run()
+graph.cypher.execute("CREATE CONSTRAINT ON (a:Airport) ASSERT a.id IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (s:State) ASSERT s.name IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (s:State) ASSERT s.abbr IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (c:City) ASSERT c.name IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (a:Aircraft) ASSERT a.tail_num IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (c:Carrier) ASSERT c.id IS UNIQUE;")
+graph.cypher.execute("CREATE CONSTRAINT ON (r:Reason) ASSERT r.name IS UNIQUE;")
+graph.cypher.execute("CREATE INDEX ON :Flight(flight_num);")
+graph.cypher.execute("CREATE INDEX ON :Airport(abbr);")
+graph.cypher.execute("CREATE INDEX ON :Carrier(abbr);")
